e685238b3049af1baef7e5415dcd150d4bfa58b7
9540anisha <101531964+9540anisha@users.noreply.github.com>
2023-05-17 16:39:45 +0000

    Modified app.py to extract keywords from uploaded PDF files

--- AutomatedEmail/app.py
+++ AutomatedEmail/app.py
@@ -1,10 +1,17 @@
+import re
+import nltk
+from nltk.corpus import stopwords
 from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
-from tempfile import mkdtemp
-import os
-from werkzeug.security import check_password_hash, generate_password_hash
 from PyPDF2 import PdfReader
-# import openai
+from collections import Counter
+# import open
+
+#global_var
+content_resume = " "
+content_prof = " "
+keywords_resume = " "
+keywords_prof = " "
 
 # Configure application
 app = Flask(__name__)
@@ -28,6 +35,42 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+# Function to extract keywords from text using a keyword list
+def extract_keywords(text, keyword_list):
+    cleaned_text = re.sub(r'[^a-zA-Z0-9\s]', '', text.lower())
+    words = cleaned_text.split()
+    keywords = []
+
+    for i, word in enumerate(words):
+        if word in keyword_list:
+            # Specify the range of subsequent words to include
+            subsequent_words = ' '.join(words[i+1:i+4])  # Include the next 3 words
+            keywords.append((word, subsequent_words))
+
+    return keywords
+
+def extract_keywords_by_num(text, num_keywords):
+    # Convert the text to lowercase
+    cleaned_text = text.lower()
+    
+    # Remove non-alphanumeric characters
+    cleaned_text = re.sub(r'[^a-zA-Z0-9\s]', '', cleaned_text)
+    
+    # Split the text into words
+    words = cleaned_text.split()
+    
+    # Remove stopwords
+    stop_words = set(stopwords.words('english'))
+    words = [word for word in words if word not in stop_words]
+    
+    # Find keyword frequencies using Counter
+    keyword_frequencies = Counter(words)
+    
+    # Extract keywords with highest frequencies
+    keywords = keyword_frequencies.most_common(num_keywords)
+    
+    return keywords
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('home.html')
@@ -37,21 +80,41 @@ def uploadResume():
     if request.method == 'POST':
         file = request.files['resume']
         pdf_reader = PdfReader(file)
-        text_resume = ''
+        content_resume = ''
         for page in pdf_reader.pages:
-            text_resume += page.extract_text()
-        return render_template('home.html', content = text_resume, prof = '')
+            content_resume += page.extract_text()
+
+        # Predefined keyword list for resume extraction
+        keywords = ['skill', 'experience', 'education', 'achievement', 'qualification', 'employ']
+
+        # Extract keywords from the extracted text
+        keywords_resume = extract_keywords(content_resume, keywords)
+
+        return render_template('home.html', content=content_resume, keywords_resume=keywords_resume, prof = content_prof, keywords_prof = keywords_prof)
     else:
         return render_template('home.html')
+    
+def extract_text_from_pdf(file):
+    pdf_reader = PdfReader(file)
+    num_pages = len(pdf_reader.pages)
+    text = ''
+
+    for page_num in range(num_pages):
+        page = pdf_reader.pages[page_num]
+        text += page.extract_text()
+
+    return text
 
 @app.route('/uploadProfessorWork', methods=['GET', 'POST'])
 def uploadProfessorWork():
     if request.method == 'POST':
         file = request.files['professorWork']
-        pdf_reader = PdfReader(file)
-        prof_work = ''
-        for page in pdf_reader.pages:
-            prof_work+= page.extract_text()
-        return render_template('home.html', prof = prof_work, content = '')
+        content_prof = extract_text_from_pdf(file)
+
+        # Extract keywords with highest frequencies
+        num_keywords = 10  # Number of keywords to extract
+        keywords_prof = extract_keywords_by_num(content_prof, num_keywords)
+
+        return render_template('home.html', content=content_resume, keywords_resume=keywords_resume, prof=content_prof, keywords_prof=keywords_prof)
     else:
         return render_template('home.html')
